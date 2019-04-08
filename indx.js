b95102994e74da74a21f24051b296f5791c7f1dd
BintangSaputra <bintangcbs26@gmail.com>
2019-04-08 16:03:04 +0700

    add files

---
+++ indx.js
@@ -0,0 +1,21 @@
+console.log(2 > 1);
+
+console.log(2 === 1);
+
+console.log("Z" > "A");
+
+console.log("1" == 1);
+
+console.log("1" === 1);
+
+//  ==================================
+const ageA = 25;
+const ageB = 50;
+
+if (ageA < ageB) {
+  console.log("A lebih muda dari B");
+} else if (ageA.ageB) {
+  console.log("tidak tepat");
+} else {
+  console.log("coba lagi");
+}
